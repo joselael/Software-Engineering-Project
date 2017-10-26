e0f031c20545f610effaf274c4fcf6070081f923
Jose L. Louis <jllouis@live.com>
2017-10-26 18:37:28 -0400

    Added login url handlers

--- website_container/website/website/urls.py
+++ website_container/website/website/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
+    url(r'^login/$', auth_views.login,{'template_name':'l' } name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
