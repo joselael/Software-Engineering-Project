7bdf2e0accea5c24dcb9a11678dade82a5076dc1
Jose L. Louis <jllouis@live.com>
2017-10-27 11:55:52 -0400

    Added view http response functions

--- website_container/website/main/views.py
+++ website_container/website/main/views.py
@@ -1,6 +1,13 @@
+from django.shortcuts import get_object_or_404, render
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return httpResponse()
+def home(request):
+    return render(request, 'main/landing.html')
+
+def login(request):
+    return render(request, 'main/login.html')
+
+def signup(request):
+    return render(request, 'main/signup.html')
