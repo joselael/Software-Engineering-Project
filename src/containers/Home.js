90b3234b1eefe7262a22cb524446902bd6d7602c
Yuan Zhou <yuanzh10@yahoo.com>
2017-11-03 00:20:22 -0400

    added carousel to the home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react';
 import LandingHeader from '../components/LandingHeader'
 import LandingFooter from '../components/LandingFooter'
+import LandingCarousel from '../components/LandingCarousel'
 
-const Home = () => (
-  <div>
-    <LandingHeader />
-    <LandingFooter />
-  </div>
-)
+class Home extends Component {
+	render() {
+		return (
+		  <div>
+		    <LandingHeader />
+		    <LandingCarousel />
+		    <LandingFooter />
+		  </div>
+  		)
+	}
+}
 
 export default Home
