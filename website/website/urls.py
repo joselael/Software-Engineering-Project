dabc9bc49acef797545236816f2b21e3788b76d3
Jose L. Louis <jllouis@live.com>
2017-11-17 12:39:24 -0500

    removed home url in urls.py for debug

--- website/website/urls.py
+++ website/website/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+   #  url(r'^$', views.home, name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'main/registration/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout,{'next_page': '/'}, name='logout'),
     # url(r'^signup/$', auth_views.signup, name='signup'),
