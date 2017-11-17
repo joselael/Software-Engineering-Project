aab97867c697a9e443ff0d159c2c9873de1f4243
Yong-L <dkrcjs7521@gmail.com>
2017-11-17 13:06:03 -0500

    Fix navbar padding

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -9,9 +9,21 @@ class NavigationBar extends Component {
       <div className="NavigationBar">
         <Navbar className="navbar-expand-lg navbar-light bg-light fixed-top">
           <NavbarBrand href="/">Simple Coding Turkish System</NavbarBrand>
-            <Nav className="ml-auto" navbar>
+            <Nav className="ml-auto navbar-nav" id="navLinks">
               <NavItem>
-                <NavLink href="/signin/">Sign In</NavLink>
+                <NavLink to='/'>Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to='/about'>About</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to='/signin'>Sign Up</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to='/signup'>Sign In</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to='/publicsearch'>Search</NavLink>
               </NavItem>
             </Nav>
         </Navbar>
