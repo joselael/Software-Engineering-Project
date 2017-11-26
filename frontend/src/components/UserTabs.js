0898dd6c186f31b4b744f77518e3913e83881514
Yong-L <dkrcjs7521@gmail.com>
2017-11-26 11:40:12 -0500

    Resize table on myaccounts page

--- frontend/src/components/UserTabs.js
+++ frontend/src/components/UserTabs.js
@@ -3,6 +3,7 @@ import { TabContent, TabPane, Nav, NavItem,
   NavLink, Button, Table,
   Row, Col } from 'reactstrap';
 import classnames from 'classnames';
+import '../css/usertab.css'
 
 export class SuperUserTab extends React.Component {
   constructor(props) {
@@ -50,69 +51,71 @@ export class SuperUserTab extends React.Component {
             </NavLink>
           </NavItem>
         </Nav>
-        <TabContent activeTab={this.state.activeTab}>
-          <TabPane tabId="1">
-            <Row>
-              <h4>Pending</h4>
-              <Table hover responsive>
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
-              </Table>
-            </Row>
-            <Row>
-              <h4>Accepted</h4>
-              <Table hover responsive>
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
-              </Table>
-            </Row>
-            <Row>
-              <h4>Blacklisted</h4>
-              <Table hover responsive>
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
-              </Table>
-            </Row>
-          </TabPane>
-          <TabPane tabId="2">
-            <Row>
-              <Col sm="6">
-                <h4>I am profile tab</h4>
-              </Col>
-              <Col sm="6">
-                <h4>My name is</h4>
-              </Col>
-            </Row>
-          </TabPane>
-          <TabPane tabId="3">
-            <Row>
-              <Col sm="12">
-                <h4>I am setting tab</h4>
-              </Col>
-            </Row>
-          </TabPane>
-        </TabContent>
+        <div className="activeTab">
+          <TabContent activeTab={this.state.activeTab}>
+            <TabPane tabId="1">
+              <Row>
+                <h4>Pending</h4>
+                <Table hover responsive>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>Username</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                </Table>
+              </Row>
+              <Row>
+                <h4>Accepted</h4>
+                <Table hover responsive>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>Username</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                </Table>
+              </Row>
+              <Row>
+                <h4>Black List</h4>
+                <Table hover responsive>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>Username</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                </Table>
+              </Row>
+            </TabPane>
+            <TabPane tabId="2">
+              <Row>
+                <Col sm="6">
+                  <h4>I am profile tab</h4>
+                </Col>
+                <Col sm="6">
+                  <h4>My name is</h4>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tabId="3">
+              <Row>
+                <Col sm="12">
+                  <h4>I am setting tab</h4>
+                </Col>
+              </Row>
+            </TabPane>
+          </TabContent>
+        </div>
       </div>
     );
   }
