64f439237cd056c54db2a03a55cee6c9732d6f1c
Jose L. Louis <jllouis@live.com>
2017-11-25 20:04:03 -0500

    update registration endpoint, accounts get endpoint, accounst update endpoint, and accounts delete endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const logger = require('morgan');
+const errorhandler = require('errohandler');
 const {check, validationResult} = require('express-validator/check');
 const {matchedData, sanitize} = require('express-validator/filter');
+const bcrypt = require('bcrypt');
+
+const port = 3000;
 
 app.use(bodyParser.json());
-app.use(morgan('dev'));
+app.use(logger('dev'));
+app.use(errorhandler());
 
 /* debug */
-const profile = [{
-    username: 'lael',
-    email: 'jllouis@live.com',
-    url: 'http://localhost:3000'
-}];
+const ddb = {}; //debug database
+ddb.accounts = [];
 
 /* logging middleware, for debugging */ 
-app.use((req, res, next)=>{
-    console.log(`${req.method}: ${req.url}`);
-    next();
-})
+// app.use((req, res, next)=>{
+//     console.log(`${req.method}: ${req.url}`);
+//     next();
+// })
 
 /* auth middleware */ 
 // app.use((req, res, next)=>{
@@ -30,27 +32,55 @@ app.use((req, res, next)=>{
 //     }
 // })
 
+// register endpoint
+app.post('/register', [
+    check('username').isAlphanumeric().withMessage("invalid username").trim(),
+    check('first_name').isAlpha().withMessage("invalid first name").trim,
+    check('last_name').isAlpha().withMessage("invalid last name").trim(),
+    check('password', 'passwords must be at least 8 chars long and contain one number')
+    .isLength({min : 5}).matches(/\d/)
+], (req, res, next) => {
 
-app.get('/profile', (req, res) => {
-    if (req.query.id) return res.send(profile[req.query.id]);
-    res.send(profile);
+    //hash pw
+    var pwhash; 
+    bcrypt.hash(req.body.password, 10, function(err, hash) {
+        pwhash = hash;
+    })
+
+    let acc_id = ddb.accounts.length; 
+
+    let user_obj = {
+        username: req.body.username,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        password: pwhash,
+        id: acc_id,
+        enabled: false /* accounts is disabled until admin enables it */
+    }
+
+    ddb.accounts.push(user_obj); /* save user into db */
+    req.status(201).send({id:acc_id});
 })
 
-app.post('/profile', (req, res)=>{
-    profile.push(req.body);
-    console.log('created', profile);
-    res.sendStatus(201);
+app.get('/login', (req, res) => {
+
+} )
+/* get user accounts, this should presumably only be callable by admin */
+app.get('/accounts', (req, res) => {
+    res.setatus(200).send(ddb.accounts);
+
 })
 
-app.put('/profile/:id', (req, res)=>{
-    Object.assign(profile[req.params.id], req.body);
-    console.log('updated', profile);
-    res.sendStatus(204);
+/* update account info, also should only be callable by admin */
+app.put('/accounts/:id', (req, res)=>{
+    store.accounts[req.params.id] = req.body;
+    res.status(200).send(store.accounts[req.params.id]);
 })
 
-app.delete('/profile', (red, res) => {
-    profile.splice(req.params.id, 1);
-    res.sendStatus(204);
+/* delete account, only callable by admin */
+app.delete('/accounts/:id', (req, res) => {
+    ddb.accounts.splice(req.params.id, 1);
+    res.status(204).send();
 })
 
-app.listen(3000);
+app.listen(port);
