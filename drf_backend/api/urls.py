0f7be1dffbe327023876f8632728e21bc77e5914
Yong-L <dkrcjs7521@gmail.com>
2017-11-26 19:43:29 -0500

    File cleanup

--- drf_backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-# file: api/urls.py
-
-from django.conf.urls import url, include
-from rest_framework.authtoken import views as drf_views
-
-urlpatterns = [
-    url(r'^auth$', drf_views.obtain_auth_token, name='auth'),
-]
