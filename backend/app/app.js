628ca12c5ab8415fc0e1f241bc658cc0417bba45
Jose L. Louis <jllouis@live.com>
2017-12-01 15:01:50 -0500

    splitting functionality to their respective files, couple typo fixes

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,14 +1,18 @@
+//SWEP: backend: app.js
+
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const errorhandler = require('errorhandler');
+//const cors = require('cors'); // to enable cross-origin requests
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
+
+// var index = require('./routes/index');
+// var users = require('./routes/users');
+var routes = require('./routes/routes');
+var db = require('./db');
+const app = express(); // initialize app
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,14 +20,17 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(errorhandler());
+app.use(cors());
+app.use(bodyParse.json());
+app.use(bodyParser.urlEnconded({extended: false}));
+
+// app.use('/', index);
+// app.use('/users', users);
+app.use('/routes', routes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
