f7d170ffcba9bf570f6f05caa588765d5c86e387
Yong-L <dkrcjs7521@gmail.com>
2017-12-02 14:18:34 -0500

    Modal for adding projects

--- frontend/src/components/UserTabs/ClientTab.js
+++ frontend/src/components/UserTabs/ClientTab.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { TabContent, TabPane, Nav, NavItem, 
-  NavLink, Button, Table,
-  Row, Col, Media } from 'reactstrap';
+    NavLink, Button, Table,
+    Row, Col, Media,
+    Modal, ModalBody, ModalFooter, ModalHeader
+} from 'reactstrap';
 import '../../css/usertab.css';
 import { accounts, acceptUser, blacklistUser, deleteUser } from '../../utils/Users'
 import store from '../../store'
@@ -13,18 +15,28 @@ export class ClientTab extends Component {
     constructor(props) {
         super(props)
 
-        this.toggle = this.toggle.bind(this);
         this.state = {
             activeTab: '1',
-            projects: []
+            projects: [],
+            modal: false
         }
+
+        this.toggleTab = this.toggleTab.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
     }
-    toggle(tab) {
+
+    toggleTab(tab) {
         if (this.state.activeTab !== tab) {
         this.setState({
             activeTab: tab
         });
         }
+    }
+
+    toggleModal() {
+        this.setState({
+            modal: !this.state.modal
+        })
     }
 
     render() {
@@ -34,7 +46,7 @@ export class ClientTab extends Component {
             <NavItem>
                 <NavLink
                 className={classnames({ active: this.state.activeTab === '1' })}
-                onClick={() => { this.toggle('1'); }}
+                onClick={() => { this.toggleTab('1'); }}
                 >
                 Projects
                 </NavLink>
@@ -42,7 +54,7 @@ export class ClientTab extends Component {
             <NavItem>
                 <NavLink
                 className={classnames({ active: this.state.activeTab === '2' })}
-                onClick={() => { this.toggle('2'); }}
+                onClick={() => { this.toggleTab('2'); }}
                 >
                 Profile
                 </NavLink>
@@ -50,7 +62,7 @@ export class ClientTab extends Component {
             <NavItem>
                 <NavLink
                 className={classnames({ active : this.state.activeTab === '3'})}
-                onClick={() => { this.toggle('3'); }}
+                onClick={() => { this.toggleTab('3'); }}
                 >
                 Settings
                 </NavLink>
@@ -60,7 +72,29 @@ export class ClientTab extends Component {
             <TabContent activeTab={this.state.activeTab}>
                 <TabPane tabId="1">
                 <Row>
-                    <h4>Current Projects</h4>
+                    <h4>
+                        Current Projects
+                    </h4>
+                    <Button
+                        color="primary"
+                        onClick={this.toggleModal}>
+                        Add Projects
+                    </Button>
+                    <Modal isOpen={this.state.modal} toggle={this.toggleModal}>
+                        <ModalHeader toggle={this.toggleModal}>Modal Header
+                            <ModalBody>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </ModalBody>
+                        </ModalHeader>
+                        <ModalFooter>
+                            <Button color="primary" onClick={this.toggleModal}>
+                                Do something
+                            </Button>
+                            <Button color="primary" onClick={this.toggleModal}>
+                                Do something else
+                            </Button>
+                        </ModalFooter>
+                    </Modal>
                     <Table hover responsive striped>
                     <thead>
                         <tr>
