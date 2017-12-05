19bc2a348615e1dbe53b81fd516291aa5232ec42
Jose L. Louis <jllouis@live.com>
2017-12-05 12:17:24 -0500

    fixed POST /user/check endpoint

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -17,15 +17,17 @@ router.get('/accounts', VerifyAdmin, (req, res) => {
     });
 });
 
+
+// check password
 router.post('/check', VerifyToken, (req, res) => {
     User.findById(req.userID, function (err, user) {
         if (err) return res.status(500).send("There was a problem finding the user.");
         if (!user) return res.status(404).send("No user found.");
 
-        if (req.body.password === user.password)
+        if (bcrypt.compareSync(req.body.password, user.password))
             res.status(200).send(true);
         else
-            res.status(200).send(false);
+            res.status(401).send(false);
     });
 });
 
