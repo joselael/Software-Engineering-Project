629d138637e6b54a895c3359a618bb7841783958
Jose L. Louis <jllouis@live.com>
2017-12-05 11:26:59 -0500

    removed money in favor of account_balance, added warnings number

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,19 +1,20 @@
 const mongoose = require('mongoose');
 
- UserSchema = new mongoose.Schema({
-  username: String,
-  first_name: String,
-  last_name: String,
-  email: String,
-  password: String,
-  user_type: String,
-  enabled: Boolean,
-  blacklisted: Boolean,
-  admin_message: String,
-  linkedIn: String,
-  github: String,
-  first_login: Boolean,
-  money: Number
+UserSchema = new mongoose.Schema({
+    username: String,
+    first_name: String,
+    last_name: String,
+    email: String,
+    password: String,
+    user_type: String,
+    account_balance: Number,
+    enabled: Boolean,
+    warnings: Number,
+    blacklisted: Boolean,
+    admin_message: String,
+    linkedIn: String,
+    github: String,
+    first_login: Boolean
 });
 
 const User = mongoose.model('User', UserSchema);
