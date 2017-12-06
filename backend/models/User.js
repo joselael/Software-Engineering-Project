5926cb337d2a1524537aa4375a0f2de4e7d1d58c
Jose L. Louis <jllouis@live.com>
2017-12-06 18:19:19 -0500

    Specified mandatory and unique fields in Schemas; still debugging /project/bid endpoint; server now launched without error.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 UserSchema = new mongoose.Schema({
-    username: String,
-    first_name: String,
-    last_name: String,
-    email: String,
-    password: String,
-    user_type: String,
+    username: {type: String, unique: true, trim: true},
+    first_name: {type: String, trim: true, required: true},
+    last_name: {type: String, trim: true, required: true},
+    email: {type: String, unique: true, trim: true, required: true},
+    password: {type: String, required: true},
+    user_type: {type: String, required: true},
     account_balance: Number,
     enabled: Boolean,
     warnings: Number,
