ca2f263cc46330244907b1ea7d38b9c962fbd7f5
Jose L. Louis <jllouis@live.com>
2017-12-07 22:28:03 -0500

    Changing fields where users have options of hiding to json ojbs with value field, and visible field.

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,10 @@ UserSchema = new mongoose.Schema({
     username: {type: String, unique: true, trim: true},
     first_name: {type: String, trim: true, required: true},
     last_name: {type: String, trim: true, required: true},
-    email: {type: String, unique: true, trim: true, required: true},
+    email: {
+        value: {type: String, unique: true, trim: true, required: true},
+        visible: {type: Boolean, trim: true, default: true, require: false}
+    },
     password: {type: String, required: true},
     user_type: {type: String, required: true},
     account_balance: Number,
@@ -12,8 +15,14 @@ UserSchema = new mongoose.Schema({
     warnings: Number,
     blacklisted: Boolean,
     admin_message: String,
-    linkedIn: String,
-    github: String,
+    linkedIn: {
+        value: {String, trim: true, required: false},
+        visible: {type: Boolean, trim: true, default: true, require: false}
+    },
+    github: {
+        value: {type: String, trim: true, require: false},
+        visible: {type: Boolean, trim: true, default: true, require: false}
+    },
     first_login: Boolean,
     delete_requested: {type: Boolean, default: false}
 });
