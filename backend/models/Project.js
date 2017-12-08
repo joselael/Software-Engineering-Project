6b95dc2d2f9da95ce519f7d751f3bd95a5042bf3
Jose L. Louis <jllouis@live.com>
2017-12-08 05:05:00 -0500

    Updating code, etc.

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Bid = require('./Bid');
-const User = require('./User')
+const User = require('./User');
 
 const ProjectSchema = new mongoose.Schema({
     title: {type: String, required: true},
@@ -10,8 +10,8 @@ const ProjectSchema = new mongoose.Schema({
     post_date: {type: Date, default: Date.now},
     bid_start: {type: Date, default: Date.now},
     bid_end: {type: Date},
-    project_end: {type: Date},
-    max_budget: Number,
+    project_end: {type: Date, required: true},
+    max_budget: {type: Number, required: true},
     bids: [{type: mongoose.Schema.Types.ObjectId, ref: 'Bid'}],
     assignee: {
         user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null},
