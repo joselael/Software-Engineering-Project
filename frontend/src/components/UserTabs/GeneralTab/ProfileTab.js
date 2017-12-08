845f91e22b94a30d944dde30b8afb973b3a66042
Yong-L <dkrcjs7521@gmail.com>
2017-12-08 02:52:40 -0500

    Schema fix

--- frontend/src/components/UserTabs/GeneralTab/ProfileTab.js
+++ frontend/src/components/UserTabs/GeneralTab/ProfileTab.js
@@ -38,7 +38,7 @@ export default class ProfileTab extends Component {
             <Media body>
             <br/>
               <Media heading style={{display: 'flex', justifyContent: 'center'}}>
-                {store.getState().user.first_name} {store.getState().user.last_name}
+                {store.getState().user.first_name} {store.getState().user.last_name.value}
               </Media>
               <div style={{display: 'flex', justifyContent: 'center'}}>
               {store.getState().user.user_type}
