0edfdc66650e3f44412039ee1acd2675a258cbbf
Jose L. Louis <jllouis@live.com>
2017-12-10 16:47:01 -0500

    removing debugging console.logs

--- backend/file/fileStorage.js
+++ backend/file/fileStorage.js
@@ -12,11 +12,6 @@ const bucket = storage.bucket('swep-bucket');
 
 upload = function (req, res, name) {
 
-    console.log("file is " + req.file);
-    console.log("name is: " + name);
-    console.log("file type is : " + req.file.mimetype);
-
-
     // Create a new blob in the bucket and upload the file data.
     const blob = bucket.file(name);
 
@@ -29,20 +24,17 @@ upload = function (req, res, name) {
         }
     });
 
-    console.log("created blob writestream");
-
     blobStream.on("error", err => {
         console.log(err);
     });
 
     blobStream.on("finish", () => {
-        console.log("on blobStream.on finish");
         // The public URL can be used to directly access the file via HTTP.
         const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
-        console.log("url is: " + publicUrl);
+
         // Make the image public to the web (since we'll be displaying it in browser)
         blob.makePublic().then(() => {
-            console.log("on blob.makepublic");
+
             // res.status(200).send(`File uploaded to ${publicUrl}`);
             res.status(201).send(`File uploaded to ${publicUrl}`);
         });
