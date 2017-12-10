9d604256504bf5b1f2c500060892aa305e58293e
Gina DiCarlo <dicarlo.g.m@gmail.com>
2017-12-10 14:10:46 -0500

    protest

---
+++ backend/routes/protest.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const Project = require('../models/Project');
+const Bid = require('../models/Bid');
+const jwt = require('jsonwebtoken');
+const VerifyAdmin = require('../auth/VerifyAdmin');
+const VerifyToken = require('../auth/VerifyToken');
+
+const bcryptSaltRounds = 10;
+const config = require('../config');
+
+
+router.put('/:id', VerifyToken, (req,res) =>{
+    User.findByIdAndUpdate(req.params.id, {$set: {protest_message: req.body.message, protest_check : true}},function(err,user){
+        if(err) res.status(500).send("There was a problem retrieving the user.");
+        res.status(200).send(user);
+    });
+});
+
+router.get('/:id', VerifyToken, (req,res) =>{
+    User.findById(req.params.id, function(err,user){
+        if(err) res.status(500).send("There was a problem retrieving the user.");
+        res.status(200).send(user.protest_message);
+    });
+});
+
+router.put('/warning_update/:id', VerifyAdmin, (req,res) =>{
+    User.findByIdAndUpdate(req.params.id, {$set : {protest_message : "", protest_check: false}}, function(err, user){
+        if(err) res.status(500).send("There was a problem updating the protest info for the user.");
+        else{
+            console.log(user);            
+            if(req.body.warning === "remove"){
+                var warning_update = user.warnings - 1;
+                user.set({ warnings: warning_update });
+                user.save(function (err, warnings) {
+                  if (err) return handleError(err);
+                  res.send(user.warnings.toString());
+        });
+        }
+        else{
+            res.status(200).send(user);
+        }
+    }
+});
+});
+
+module.exports = router;
