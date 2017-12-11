99780ae362ee352235bd2bd0c8e55eec9f9df844
Jose L. Louis <jllouis@live.com>
2017-12-10 19:17:57 -0500

    removing unused imports, commented out code

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-//const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -12,8 +11,6 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
